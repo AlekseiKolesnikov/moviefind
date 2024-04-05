7b95e017b05352c49e0b410ccf2760dd3671070a
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2024-04-04 19:55:29 -0500

    The adaptive sider and navbar were made

--- src/components/layout/AppLayout/AppLayout.tsx
+++ src/components/layout/AppLayout/AppLayout.tsx
@@ -4,16 +4,14 @@ import { AppHeader } from "../AppHeader/AppHeader.tsx";
 import { AppSider } from "../AppSider/AppSider.tsx";
 import { AppContent } from "../AppContent/AppContent.tsx";
 import { AppFooter } from "../AppFooter/AppFooter.tsx";
-import { useWindowSize } from "../../../states/windowSizeState.ts";
 
 export const AppLayout = () => {
-    const isDesktop = useWindowSize((state) => state.isDesktopScreen)
 
     return (
         <div className="layout-wrapper center-column-top-flex">
             <AppHeader/>
             <div className="layout-content-sider-container center-row-flex">
-                {isDesktop && <AppSider/>}
+                <AppSider/>
                 <AppContent/>
             </div>
             <AppFooter/>
--- src/states/windowSizeState.ts
+++ src/states/windowSizeState.ts
@@ -3,11 +3,21 @@ import { create } from "zustand";
 type WindowSize = {
     isDesktopScreen: boolean,
     isTabletScreen: boolean,
-    isMobileScreen: boolean
+    isMobileScreen: boolean,
+    setWindowSize: () => void
 }
 
-export const useWindowSize = create<WindowSize>(() => ({
+export const useWindowSize = create<WindowSize>((set) => ({
     isDesktopScreen: window.innerWidth > 1300,
     isTabletScreen: window.innerWidth > 1000 && window.innerWidth < 1300,
-    isMobileScreen: window.innerWidth < 450
+    isMobileScreen: window.innerWidth < 450,
+    setWindowSize: () => set(() => ({
+        isDesktopScreen: window.innerWidth > 1300,
+        isTabletScreen: window.innerWidth > 1000 && window.innerWidth < 1300,
+        isMobileScreen: window.innerWidth < 450
+    }))
 }))
+
+window.addEventListener('resize', () => {
+    useWindowSize.getState().setWindowSize();
+});
